82ef0e6bf0c9627bffe9b5a246659a6be8f2e11f
juyeongjung <juyeong.dev@gmail.com>
2020-04-23 11:06:06 +0900

    spring -mvc 실습

--- src/main/java/wooteco/helloworld/controller/WelcomeController.java
+++ src/main/java/wooteco/helloworld/controller/WelcomeController.java
@@ -1,34 +1,44 @@
 package wooteco.helloworld.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
 import wooteco.helloworld.domain.User;
 
+@Controller
 public class WelcomeController {
-    /**
-     * GET /welcome
-     */
-    public String welcome() {
-        return "welcome";
-    }
+	/**
+	 * GET /welcome
+	 */
+	@GetMapping("/welcome")
+	@ResponseBody
+	public String welcome() {
+		return "welcome";
+	}
 
-    /**
-     * GET /welcome-page
-     */
-    public String welcomePage() {
-        return "welcome";
-    }
+	/**
+	 * GET /welcome-page
+	 */
+	@GetMapping("/welcome-page")
+	public String welcomePage(Model model, @RequestParam(defaultValue = "") String name) {
+		model.addAttribute("name", name);
+		return "welcome";
+	}
 
-    /**
-     * GET /welcome-user
-     */
-    public ResponseEntity welcomeUser() {
-        return ResponseEntity.ok(new User("브라운", "brown@email.com", 20));
-    }
+	/**
+	 * GET /welcome-user
+	 */
+	@GetMapping("/welcome-user")
+	public ResponseEntity welcomeUser() {
+		return ResponseEntity.ok(new User("브라운", "brown@email.com", 20));
+	}
 
-    @GetMapping("/post-form")
-    public String postForm() {
-        return "post_form";
-    }
+	@GetMapping("/post-form")
+	public String postForm() {
+		return "post-form";
+	}
 }
